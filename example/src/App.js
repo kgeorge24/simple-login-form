664d651c71cb8807b499f4dadcec167062d0b59a
Kyle George <k123yge@live.com>
2020-06-19 22:42:17 -0400

    publishing

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react'
-
-import { ExampleComponent } from 'simple-login-form'
+import React, { Component } from 'react'
+// Example of importing an image for the photo icon.
+import picture from './glass.jpg'
+import { SimpleLoginForm } from 'simple-login-form'
 import 'simple-login-form/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+export default class App extends Component {
+  sendUserInfoToDatabase = (userInfo, event) => {
+    // Write your code here...
+  }
 
-export default App
+  // Default styling supplied. You can change the background color,
+  // border radius and the width. Height is advised to be left as is.
+  formStyle = {
+    maxWidth: '350px',
+    maxHeight: '525px',
+    background: 'linear-gradient(#e66465, #9198e5)',
+    borderRadius: '2%'
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <SimpleLoginForm
+          getUserInfo={this.sendUserInfoToDatabase}
+          style={this.formStyle}
+          photoIcon={picture}
+        />
+      </React.Fragment>
+    )
+  }
+}
